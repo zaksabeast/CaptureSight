1a095991fc7e303e71128bce2cadfd77672c2554
zaksabeast <19464008+zaksabeast@users.noreply.github.com>
2020-02-13 07:06:38 -0600

    Initialize lookups outside of headers

--- libcsight/include/csight/lookups/Natures.hpp
+++ libcsight/include/csight/lookups/Natures.hpp
@@ -3,7 +3,4 @@
 #include <vector>
 #include <string>
 
-std::vector<std::string> NatureList = {
-    "Hardy", "Lonely", "Brave",  "Adamant", "Naughty", "Bold",    "Docile", "Relaxed", "Impish", "Lax",   "Timid",   "Hasty",  "Serious",
-    "Jolly", "Naive",  "Modest", "Mild",    "Quiet",   "Bashful", "Rash",   "Calm",    "Gentle", "Sassy", "Careful", "Quirky",
-};
+extern std::vector<std::string> NatureList;
---
+++ libcsight/source/csight/lookups/Natures.cpp
@@ -0,0 +1,7 @@
+#include <vector>
+#include <string>
+
+std::vector<std::string> NatureList = {
+    "Hardy", "Lonely", "Brave",  "Adamant", "Naughty", "Bold",    "Docile", "Relaxed", "Impish", "Lax",   "Timid",   "Hasty",  "Serious",
+    "Jolly", "Naive",  "Modest", "Mild",    "Quiet",   "Bashful", "Rash",   "Calm",    "Gentle", "Sassy", "Careful", "Quirky",
+};
