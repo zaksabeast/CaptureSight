0468223c486b904bba3bf3f00eaa26ca4313f115
Uluç Şengil <u.sengil@mail.utoronto.ca>
2020-03-07 09:34:24 -0500

    show shiny sprites on lists and summary screen

--- CaptureSight/source/ui/PokemonSprite.cpp
+++ CaptureSight/source/ui/PokemonSprite.cpp
@@ -1,15 +1,16 @@
 #include <pu/Plutonium>
 #include <ui/PokemonSprite.hpp>
 
-void setPokemonSprite(pu::ui::elm::Image::Ref sprite, u8 scale, u16 species, bool isEgg) {
-  std::string spritePath = getPokemonIconPath(species, isEgg);
+void setPokemonSprite(pu::ui::elm::Image::Ref sprite, u8 scale, u16 species, bool isEgg, bool isShiny) {
+  std::string spritePath = getPokemonIconPath(species, isEgg, isShiny);
 
   sprite->SetImage(spritePath);
   sprite->SetHeight(sprite->GetHeight() * scale);
   sprite->SetWidth(sprite->GetWidth() * scale);
 }
 
-std::string getPokemonIconPath(u16 species, bool isEgg) {
+std::string getPokemonIconPath(u16 species, bool isEgg, bool isShiny) {
   std::string icon = isEgg ? "0-1" : std::to_string(species);
-  return "romfs:/pokemon-sprites/b_" + icon + ".png";
+  std::string s = (isShiny && !isEgg) ? "s" : "";
+  return "romfs:/pokemon-sprites/b_" + icon + s ".png";
 }
