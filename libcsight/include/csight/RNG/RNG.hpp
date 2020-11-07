c84958dae6f63cd01c306b82c25e4e5ce66a75fa
Admiral-Fish <admiralfish420@gmail.com>
2020-11-07 14:59:41 -0700

    Improve RNG (#58)

--- libcsight/include/csight/RNG/RNG.hpp
+++ libcsight/include/csight/RNG/RNG.hpp
@@ -1,26 +1,37 @@
 #pragma once
 
 #include <csight/RNG/IRNG.hpp>
-#include <csight/RNG/MersenneTwister.hpp>
-#include <csight/RNG/XOROSHIRO.hpp>
-#include <switch.h>
-
-using swsh_xoroshiro = xoroshiro_detail::xoroshiro_plus<uint64_t, uint64_t, 24, 16, 37>;
 
 namespace csight::rng {
   namespace lcrng {
-    u32 advance(u32 seed);
+    inline u32 advance(u32 seed) { return seed * 0x41C64E6D + 0x6073; }
   }
 
   class xoroshiro : public IRNG {
    public:
     using IRNG::next;
 
-    xoroshiro(ulong seed);
-    u64 nextulong();
-    u32 next();
+    xoroshiro(u64 seed) : state0(seed), state1(0x82A2B175229D6A5B) {}
+
+    u64 nextulong() {
+      u64 s0 = state0;
+      u64 s1 = state1;
+      u64 result = s0 + s1;
+
+      auto rotl = [](u64 num, int shift) {
+        return (num << shift) | (num >> (64 - shift)); 
+      };
+
+      s1 ^= s0;
+      state0 = rotl(s0, 24) ^ s1 ^ (s1 << 16);
+      state1 = rotl(s1, 37);
+
+      return result;
+    }
+
+    u32 next() { return (u32)nextulong(); }
 
    private:
-    swsh_xoroshiro m_prng;
+    u64 state0, state1;
   };
 }
