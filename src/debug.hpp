aec6120bdc37eb6b30f5803dd91be501a5e2a9df
zaksabeast <19464008+zaksabeast@users.noreply.github.com>
2021-11-03 19:24:13 -0500

    Remove unused dbg functions

--- src/debug.hpp
+++ src/debug.hpp
@@ -10,17 +10,10 @@ DmntCheatProcessMetadata g_metadata;
 namespace dbg {
   Result Initialize() {
     dmntchtInitialize();
-    dmntchtForceOpenCheatProcess();
     return dmntchtGetCheatProcessMetadata(&g_metadata);
   }
 
   void Exit() { dmntchtExit(); }
-
-  void GetCheatProcessMetadata(DmntCheatProcessMetadata *metadata) { *metadata = g_metadata; }
-
-  u64 GetCheatProcessHeapBase() { return g_metadata.heap_extents.base; }
-
-  u64 GetCheatProcessNsoBase() { return g_metadata.main_nso_extents.base; }
 
   u64 GetCheatProcessTitleId() { return g_metadata.title_id; }
 
@@ -31,34 +24,10 @@ namespace dbg {
   }
 
   template <size_t const DATA_SIZE>
-  std::array<u8, DATA_SIZE> ReadCheatProcessNso(u64 offset) {
-    std::array<u8, DATA_SIZE> result = { 0 };
-    u64 address = GetCheatProcessNsoBase() + offset;
-    dmntchtReadCheatProcessMemory(address, (void *)result.data(), DATA_SIZE);
-
-    return result;
-  }
-
-  Result WriteCheatProcessNso(u64 offset, const void *buffer, size_t size) {
-    u64 address = GetCheatProcessNsoBase() + offset;
-    return dmntchtWriteCheatProcessMemory(address, buffer, size);
-  }
-
-  template <size_t const DATA_SIZE>
   std::array<u8, DATA_SIZE> ReadCheatProcessHeap(u64 offset) {
     std::array<u8, DATA_SIZE> result = { 0 };
-    u64 address = GetCheatProcessHeapBase() + offset;
+    u64 address = g_metadata.heap_extents.base + offset;
     dmntchtReadCheatProcessMemory(address, (void *)result.data(), DATA_SIZE);
-
     return result;
   }
-
-  Result WriteCheatProcessHeap(u64 offset, const void *buffer, size_t size) {
-    u64 address = GetCheatProcessHeapBase() + offset;
-    return dmntchtWriteCheatProcessMemory(address, buffer, size);
-  }
-
-  Result PauseCheatProcess() { return dmntchtPauseCheatProcess(); }
-
-  Result UnpauseCheatProcess() { return dmntchtResumeCheatProcess(); }
 }
