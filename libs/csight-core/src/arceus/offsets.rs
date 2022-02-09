5a3276aa9afb47d37e982730ebb3a9fdac7a53f7
Steve Cook <steviecook210@gmail.com>
2022-02-09 02:58:28 -0500

    PLA 0.0.2.0

--- libs/csight-core/src/arceus/offsets.rs
+++ libs/csight-core/src/arceus/offsets.rs
@@ -3,7 +3,7 @@ use num_enum::IntoPrimitive;
 #[derive(Clone, Copy, Debug, PartialEq, IntoPrimitive)]
 #[repr(u64)]
 pub enum Offset {
-    PlayerSingleton = 0x4268000,
-    BattleSingleton = 0x4267f00,
-    SpawnSingleton = 0x4267ee0,
+    PlayerSingleton = 0x4269000,
+    BattleSingleton = 0x4268f00,
+    SpawnSingleton = 0x4268ee0,
 }
