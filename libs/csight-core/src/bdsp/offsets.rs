5ad8de023c3afaa5f76bdd9a8d378e0c2b2f6bf7
Lusamine <30205550+Lusamine@users.noreply.github.com>
2022-02-21 20:05:12 -0800

    Update pointers for BDSP version 1.2.0

--- libs/csight-core/src/bdsp/offsets.rs
+++ libs/csight-core/src/bdsp/offsets.rs
@@ -3,8 +3,8 @@ use num_enum::IntoPrimitive;
 #[derive(Clone, Copy, Debug, PartialEq, IntoPrimitive)]
 #[repr(u64)]
 pub enum Offset {
-    PlayerPrefsProviderInstance = 0x4e853f0,
-    FieldManager = 0x4e4f730,
-    MainRng = 0x4fb2050,
-    UnionRoomManager = 0x4e5c790,
+    PlayerPrefsProviderInstance = 0x4E61DD0,
+    FieldManager = 0x4E2C6F0,
+    MainRng = 0x4F8E750,
+    UnionRoomManager = 0x4E395B0,
 }
