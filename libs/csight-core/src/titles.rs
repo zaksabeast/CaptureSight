1cc3054f83e6113c6dd12c7a4a53c916f76f50ca
zaksabeast <19464008+zaksabeast@users.noreply.github.com>
2022-03-16 11:01:38 -0500

    Add convenience equality for SupportedGame

--- libs/csight-core/src/titles.rs
+++ libs/csight-core/src/titles.rs
@@ -9,3 +9,15 @@ pub enum SupportedGame {
     ShiningPearl = 0x010018E011D92000,
     Arceus = 0x01001F5010DFA000,
 }
+
+impl PartialEq<SupportedGame> for u64 {
+    fn eq(&self, other: &SupportedGame) -> bool {
+        *self == *other as u64
+    }
+}
+
+impl PartialEq<u64> for SupportedGame {
+    fn eq(&self, other: &u64) -> bool {
+        *self as u64 == *other
+    }
+}
